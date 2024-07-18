77c6d172c86f6870f794a952b158d0d82d45326d
svywx <svywxz@gmail.com>
2024-07-18 18:05:57 +0300

    setting up postgres v4

--- src/test/java/com/catJam/Order/OrderApplicationTests.java
+++ src/test/java/com/catJam/Order/OrderApplicationTests.java
@@ -1,13 +1,13 @@
-package com.catJam.Order;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OrderApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.catJam.Order;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class OrderApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
