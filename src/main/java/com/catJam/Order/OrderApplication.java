f80cd341ac92ef8e4e21632e44a9b9e908994ff6
svywx <svywxz@gmail.com>
2024-07-19 14:39:00 +0300

    initial commit

--- src/main/java/com/catJam/Order/OrderApplication.java
+++ src/main/java/com/catJam/Order/OrderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 @OpenAPIDefinition(info = @Info(title = "Orders DB", description = "Microservice for Orders data"))
 public class OrderApplication {
 
