f7dd073d4a9caeb608f42815a0112ac02bf84e5d
iBreadd <ismailsunay0@gmail.com>
2024-10-25 18:04:42 +0300

    Configured FeignClient, improved methods and controller.

--- src/main/java/com/catJam/Order/order/OrderEntity.java
+++ src/main/java/com/catJam/Order/order/OrderEntity.java
@@ -1,42 +1,29 @@
 package com.catJam.Order.order;
 
-
-import com.catJam.Order.bookClient.BookModel;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
-
 @Entity
 @Table(name = "orders")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
 
-    private Long userId;
+    @Column(nullable = false)
+    private String customerName;
 
-    private double totalAmount;
+    @Column(nullable = false)
+    private Long bookId;
 
-    @CreationTimestamp
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
     private LocalDate orderDate;
-
-    @ElementCollection
-    @CollectionTable(name = "order_books", joinColumns = @JoinColumn(name = "order_id"))
-    @MapKeyColumn(name = "book_id")
-    @Column(name = "quantity")
-    private Map<Long, Integer> bookQuantities;
-
-    @Transient
-    private List<BookModel>books;
 }
 
